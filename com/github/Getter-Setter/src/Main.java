7d337f7908ac43e8eccc7c329cd104c15e8d9f10
parisaaabdollahi <77579910+parisaaabdollahi@users.noreply.github.com>
2022-06-14 14:00:54 +0430

    Getter-Setter

---
+++ com/github/Getter-Setter/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+   Manager manager = new Manager();
+   manager.setName("parisa");
+   manager.setFamily("Abdollahi");
+   manager.setId(12564l);
+
+
+        System.out.println(manager.getName()+manager.getFamily()+manager.getId());
+    }
+}
---
+++ com/github/Getter-Setter/src/Manager.java
@@ -0,0 +1,32 @@
+public class Manager {
+    private String name;
+    private String family;
+    private Long id;
+
+    
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
