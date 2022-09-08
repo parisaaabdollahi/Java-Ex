2bbf586f7a9665f5beb9feeb470cac10f596ebfd
parisaaabdollahi <77579910+parisaaabdollahi@users.noreply.github.com>
2022-09-08 16:07:42 +0430

    Create Time

--- JakartaEE/com/github/check-in-out-system/src/entity/Time.java
+++ JakartaEE/com/github/check-in-out-system/src/entity/Time.java
@@ -3,5 +3,34 @@ package entity;
 import java.io.Serializable;
 
 public class Time implements Serializable {
-    
+    private String employeeCode;
+    private Float checkIn;
+    private Float getCheckout;
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public Time setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+        return this;
+    }
+
+    public Float getCheckIn() {
+        return checkIn;
+    }
+
+    public Time setCheckIn(Float checkIn) {
+        this.checkIn = checkIn;
+        return this;
+    }
+
+    public Float getGetCheckout() {
+        return getCheckout;
+    }
+
+    public Time setGetCheckout(Float getCheckout) {
+        this.getCheckout = getCheckout;
+        return this;
+    }
 }
